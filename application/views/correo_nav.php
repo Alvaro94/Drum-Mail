2bc9553241297eec409bb8ebec4c57d8eb5de640
Alvaro94 <alvchasa_94@hotmail.es>
2016-04-06 00:18:33 -0600

    arreglo css
    nav y background en bandejas

--- application/views/correo_nav.php
+++ application/views/correo_nav.php
@@ -1,3 +1,6 @@
+<link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/style_email.css" media="all">
+
+
 <?php if (isset($this->session->userdata['logged_in'])) {
 	$username = ($this->session->userdata['logged_in']['username']);
 	$email = ($this->session->userdata['logged_in']['email']);
@@ -12,19 +15,19 @@
                <span class="icon-bar"></span>
                <span class="icon-bar"></span>
                </button>
-               <a class="navbar-brand" href="#">Drum-Mail</a>
+               <a id="nav" class="navbar-brand" href="#">Drum-Mail</a>
           </div>
 <!-- menu section -->
           <div class="collapse navbar-collapse navbar-ex1-collapse">
                <ul class="nav navbar-nav navbar-left">
 
-               <li role="presentation" class="active"><a href="#salida" aria-controls="salida" role="tab" data-toggle="tab">Salida</a></li>
+               <li ole="presentation" class="active"><a id="nav"rhref="#salida" aria-controls="salida" role="tab" data-toggle="tab">Salida</a></li>
                 <li role="presentation"><a href="#enviados" aria-controls="enviados" role="tab" data-toggle="tab">Enviados</a></li>
 
            </ul>
                <ul class="nav navbar-nav navbar-right">
-               <li><a>Bienvenido <?php echo $username ?></a></li>
-               <li><a href="<?php echo base_url('user/logout') ?>">Salir</a></li>
+               <li><a id="nav">Bienvenido <?php echo $username ?></a></li>
+               <li><a id="nav" href="<?php echo base_url('user/logout') ?>">Salir</a></li>
                </ul>
           </div>
      </div>
